c6558a58da7415c92c4acdf556857c4fda4b8323
league <league@imac-12.attlocal.net>
2023-07-28 16:54:33 -0700

    LeagueInvaders (IP)

---
+++ src/GamePanel.java
@@ -0,0 +1,104 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+	int currentState = MENU;
+	Font titleFont;
+	Font smallerFont;
+	Timer frameDraw; 
+	GamePanel(){
+		titleFont = new Font("Arial", Font.PLAIN, 48);
+		smallerFont = new Font("Arial", Font.PLAIN, 12);
+		frameDraw = new Timer(1000/60, this);
+		frameDraw.start();
+	}
+	@Override
+	public void paintComponent(Graphics g){
+		if(currentState == MENU){
+			drawMenuState(g);
+		}else if(currentState == GAME){
+			drawGameState(g);
+		}else if(currentState == END){
+			drawEndState(g);
+		}
+	}
+	void updateMenuState() {  }
+	void updateGameState() {  }
+	void updateEndState()  {  }
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);  
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("LEAGUE INVADERS", 25, 100);
+		g.setFont(smallerFont);
+		g.drawString("Press ENTER to start...", 178, 300);
+		g.drawString("Press SPACE for instructions", 163, 400);
+	}
+	void drawGameState(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+	void drawEndState(Graphics g)  {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("GAME OVER", 85, 100);
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		//System.out.println("action");
+		repaint();
+		if(currentState == MENU){
+		    updateMenuState();
+		}else if(currentState == GAME){
+		    updateGameState();
+		}else if(currentState == END){
+		    updateEndState();
+		}
+	}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getKeyCode()==KeyEvent.VK_ENTER) {
+		    if (currentState == END) {
+		        currentState = MENU;
+		    } else {
+		        currentState++;
+		    }
+		}   
+		if(currentState == GAME) {
+			if (arg0.getKeyCode()==KeyEvent.VK_UP) {
+			    System.out.println("UP");
+			}else if (arg0.getKeyCode()==KeyEvent.VK_DOWN) {
+			    System.out.println("DOWN");
+			}else if (arg0.getKeyCode()==KeyEvent.VK_LEFT) {
+			    System.out.println("LEFT");
+			}else if (arg0.getKeyCode()==KeyEvent.VK_RIGHT) {
+			    System.out.println("RIGHT");
+			}
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,23 @@
+import javax.swing.*;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	GamePanel panel;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	public static void main(String[] args) {
+		LeagueInvaders spaceInvader = new LeagueInvaders();
+		spaceInvader.setup();
+	}	
+	LeagueInvaders() {
+		frame = new JFrame();
+		panel = new GamePanel();
+	}
+	void setup() {
+		frame.add(panel);
+		frame.addKeyListener(panel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
